85da9540443f5c58af5cec4c7e83d8bf9db2bc38
karanrony4 <karanrony4@gmail.com>
2026-09-22 13:26:50 +0530

    Add admin.html and configure /admin rewrite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         login: resolve(import.meta.dirname, "index.html"),
         home: resolve(import.meta.dirname, "home.html"),
         home_page: resolve(import.meta.dirname, "home-page.html"),
-        admin_app: resolve(import.meta.dirname, "admin-app.html")
+        admin_app: resolve(import.meta.dirname, "admin-app.html"),
+        admin: resolve(import.meta.dirname, "admin.html")
       }
     }
   }
